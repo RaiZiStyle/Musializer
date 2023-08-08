26919dfab3adc1d22ab02557a386912202134d5e
Arthur Guyot--Premel <arthur.guyotpremel@gmail.com>
2023-08-08 11:09:25 +0200

    It's bugging :(

--- main.c
+++ main.c
@@ -9,19 +9,25 @@
 
 #define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
 
-uint32_t global_frames[1024] = {};  // Contain 2 channels
+uint32_t global_frames[4410] = {};  // Contain 2 channels
 size_t global_frames_count = 0;
 
 // Used to take the current sound wave (or frames, or sample) and copy it to global_frames.
+// QUESTION: Why does *bufferData is Empty ?  it point to nothing
 void callback(void *bufferData, unsigned int frames) {
-    // Size we have a fixed size of global frames, we need to check if the frames will fit in global_frames[]
-    if (frames > ARRAY_LEN(global_frames)) {
-        frames = ARRAY_LEN(global_frames);
+    size_t capacity = ARRAY_LEN(global_frames);
+    // Used to do kinda a Ring buffer
+    if (frames <= capacity - global_frames_count) {
+        memcpy(global_frames + global_frames_count, bufferData, sizeof(uint32_t) * frames);
+        global_frames_count += frames;
+        // Shifting doing here.
+    } else if (frames <= capacity) {
+        memmove(global_frames, bufferData +  frames, sizeof(uint32_t) * (capacity - frames));
+        memcpy(global_frames + (capacity - frames), bufferData, sizeof(uint32_t) * frames);
+    } else {
+        memcpy(global_frames, bufferData, sizeof(uint32_t) * capacity);
+        global_frames_count = capacity;
     }
-    // QUESTION: Why does *bufferData is Empty ?  it point to nothing
-    memcpy(global_frames, bufferData, sizeof(uint32_t) * frames);
-    global_frames_count = frames;
-    // printf("Callback(frames = %u)\n", frames);
 }
 
 int main() {
@@ -30,6 +36,7 @@ int main() {
 
     InitAudioDevice();
     Music music = LoadMusicStream("file_example.ogg");
+    // Music music = LoadMusicStream("Example.ogg");
 
     assert(music.stream.sampleSize == 16);
     assert(music.stream.channels == 2);
@@ -61,17 +68,19 @@ int main() {
         BeginDrawing();
         ClearBackground(CLITERAL(Color){0x18, 0x18, 0x18, 0xFF});
 
-        float cell_width = (float) w / global_frames_count; // Get cell Width based on global_frames
+        float cell_width = (float)w / global_frames_count;  // Get cell Width based on global_frames
         for (size_t i = 0; i < global_frames_count; i++) {
             // Size global_frames is 32bits, this will take either one of the channel and take a sample
             int16_t sample = *(int16_t *)&global_frames[i];
             // The normalizeation. We need to map the value of the sample based on the sreens.
-            float t = (float)sample/INT16_MAX; 
+            // Drawing Positif sample
             if (sample > 0) {
-                DrawRectangle( i *cell_width , h / 2 -  h /2*t, cell_width , h/2*t , RED);
-            }else {
-                float t = (float)sample/INT16_MIN;
-                DrawRectangle( i * cell_width , h/2, cell_width , h/2*t, RED);
+                float t = (float)sample / INT16_MAX;
+                DrawRectangle(i * cell_width, h / 2 - h / 2 * t, 1, h / 2 * t, RED);
+                // Drawing Negatif sample
+            } else {
+                float t = (float)sample / INT16_MIN;
+                DrawRectangle(i * cell_width, h / 2, 1, h / 2 * t, RED);
             }
             // printf("%d ", sample);
         }
