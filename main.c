e18456985341a1dfd215b3a61e8b01453bbc80d1
Arthur Guyot--Premel <arthur.guyotpremel@gmail.com>
2023-08-08 10:28:21 +0200

    Starting to look like somethings

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ void callback(void *bufferData, unsigned int frames) {
     if (frames > ARRAY_LEN(global_frames)) {
         frames = ARRAY_LEN(global_frames);
     }
+    // QUESTION: Why does *bufferData is Empty ?  it point to nothing
     memcpy(global_frames, bufferData, sizeof(uint32_t) * frames);
     global_frames_count = frames;
     // printf("Callback(frames = %u)\n", frames);
@@ -44,6 +45,7 @@ int main() {
     SetMusicVolume(music, 1);
     AttachAudioStreamProcessor(music.stream, callback);
     while (!WindowShouldClose()) {
+        UpdateMusicStream(music);
         if (IsKeyPressed(KEY_SPACE)) {
             if (IsMusicStreamPlaying(music)) {
                 PauseMusicStream(music);
@@ -53,18 +55,27 @@ int main() {
             }
         }
 
-        UpdateMusicStream(music);
+        int w = GetRenderWidth();
+        int h = GetRenderHeight();
 
         BeginDrawing();
         ClearBackground(CLITERAL(Color){0x18, 0x18, 0x18, 0xFF});
 
-        float cell_width = (float)GetRenderWidth() / global_frames_count;
+        float cell_width = (float) w / global_frames_count; // Get cell Width based on global_frames
         for (size_t i = 0; i < global_frames_count; i++) {
             // Size global_frames is 32bits, this will take either one of the channel and take a sample
-            int16_t samples = *(int16_t *)&global_frames[i];
-            printf("%d ", samples);
+            int16_t sample = *(int16_t *)&global_frames[i];
+            // The normalizeation. We need to map the value of the sample based on the sreens.
+            float t = (float)sample/INT16_MAX; 
+            if (sample > 0) {
+                DrawRectangle( i *cell_width , h / 2 -  h /2*t, cell_width , h/2*t , RED);
+            }else {
+                float t = (float)sample/INT16_MIN;
+                DrawRectangle( i * cell_width , h/2, cell_width , h/2*t, RED);
+            }
+            // printf("%d ", sample);
         }
-        printf("\n");
+        // printf("\n");
         // if (global_frames_count > 0 ) exit(1);
 
         EndDrawing();
